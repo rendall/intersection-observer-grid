36231b19b19102a72da6713e8194eca5bc2f7692
Rendall <rendall@gmail.com>
2021-07-11 21:39:16 +0300

    Complete VirtualGrid

--- src/components/VirtualGrid.tsx
+++ src/components/VirtualGrid.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+const NUM = 10000
+
 type CellProps = {
   i: number;
   observer: IntersectionObserver;
@@ -18,14 +20,13 @@ const Cell = (props: CellProps) => {
       id={id}
       ref={observeRef(props.observer)}
       className={"cell"}
-      key={props.i}
     >
-      {props.isInView ? <p>is in view</p> : <p>{props.i}</p>}
+      {props.isInView && <><img className="image-display" src="https://source.unsplash.com/random/160x160" /><p className="number-display">{props.i}</p></>}
     </article>
   );
 };
 
-const ascendingArray = new Array(100).fill(0).map((c, i) => i);
+const ascendingArray = new Array(NUM).fill(0).map((c, i) => i);
 const getObserver = (callback: IntersectionObserverCallback) =>
   new IntersectionObserver(callback, {
     root: document.querySelector(".view"),
@@ -33,33 +34,21 @@ const getObserver = (callback: IntersectionObserverCallback) =>
     threshold: 1.0,
   });
 
-
-  const updateState = (state:{isInView:boolean[]}, entries:IntersectionObserverEntry[]) => {
-
-    // replace `isInView` elements with those in entries 
-    const replaceWithEntry = ( i:number, status:boolean, entries:IntersectionObserverEntry[]) => {
-      const entry = entries.find( entry => entry.target.id === `${i}`)
-      if (entry) return entry.isIntersecting
-      else return status
-    }
-
-    const isInView = state.isInView.map( (status, i) => replaceWithEntry(i, status, entries))
-    return {...state, isInView}
+const updateState = (state: { isInView: boolean[] }, entries: IntersectionObserverEntry[]) => {
+  const replaceWithEntry = (i: number, status: boolean, entries: IntersectionObserverEntry[]) => {
+    const entry = entries.find(entry => entry.target.id === `${i}`)
+    if (entry) return entry.isIntersecting
+    else return status
   }
+
+  const isInView = state.isInView.map((status, i) => replaceWithEntry(i, status, entries))
+  return { ...state, isInView }
+}
 export class VirtualGrid extends React.Component<{}, { isInView: boolean[] }> {
   observer: IntersectionObserver;
   callback: IntersectionObserverCallback = (entries, observer) => {
-
     const newState = updateState(this.state, entries)
-
-    console.log({ entries, newState, state: this.state });
-    
-    this.state = newState;
-    // entries.forEach((entry) =>
-    //   entry.isIntersecting
-    //     ? entry.target.classList.add("is-view")
-    //     : entry.target.classList.remove("is-view")
-    // );
+    this.setState(newState);
   };
 
   constructor(props: {}) {
@@ -70,12 +59,15 @@ export class VirtualGrid extends React.Component<{}, { isInView: boolean[] }> {
 
   cells = () =>
     ascendingArray.map((i) => (
-      <Cell
-        isInView={this.state.isInView[i]}
-        observer={this.observer}
-        i={i}
-        key={i}
-      />
+      <>
+        {(i % 100) === 0 && <h2 key={`h${i}`}>Header {i}</h2>}
+        <Cell
+          isInView={this.state.isInView[i]}
+          observer={this.observer}
+          i={i}
+          key={`${i}`}
+        />
+      </>
     ));
 
   render() {
