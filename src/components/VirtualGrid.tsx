c1774601d84ea5ccc4e8113372146715a42815d9
Rendall Koski <rendall@gmail.com>
2021-07-11 10:30:15 +0300

    WIP: add VirtualGrid

--- src/components/VirtualGrid.tsx
+++ src/components/VirtualGrid.tsx
@@ -1,17 +1,84 @@
-import * as React from 'react';
+import * as React from "react";
 
-const cells = (new Array(100)).fill(0).map( ( e, i ) => <article className={"cell"} key={i}>{i}</article>)
+type CellProps = {
+  i: number;
+  observer: IntersectionObserver;
+  isInView: boolean;
+};
 
-export class VirtualGrid extends React.Component {
-  state: { cells: { i: number }[] } = {cells:[]}
-  constructor(props: any) {
-    super(props);
+const Cell = (props: CellProps) => {
+  const observeRef = (observer: IntersectionObserver) =>
+    React.useCallback((node) => {
+      observer.observe(node);
+    }, []);
+  const id = `${props.i}`;
+
+  return (
+    <article
+      id={id}
+      ref={observeRef(props.observer)}
+      className={"cell"}
+      key={props.i}
+    >
+      {props.isInView ? <p>is in view</p> : <p>{props.i}</p>}
+    </article>
+  );
+};
+
+const ascendingArray = new Array(100).fill(0).map((c, i) => i);
+const getObserver = (callback: IntersectionObserverCallback) =>
+  new IntersectionObserver(callback, {
+    root: document.querySelector(".view"),
+    rootMargin: "0%",
+    threshold: 1.0,
+  });
+
+
+  const updateState = (state:{isInView:boolean[]}, entries:IntersectionObserverEntry[]) => {
+
+    // replace `isInView` elements with those in entries 
+    const replaceWithEntry = ( i:number, status:boolean, entries:IntersectionObserverEntry[]) => {
+      const entry = entries.find( entry => entry.target.id === `${i}`)
+      if (entry) return entry.isIntersecting
+      else return status
+    }
+
+    const isInView = state.isInView.map( (status, i) => replaceWithEntry(i, status, entries))
+    return {...state, isInView}
   }
+export class VirtualGrid extends React.Component<{}, { isInView: boolean[] }> {
+  observer: IntersectionObserver;
+  callback: IntersectionObserverCallback = (entries, observer) => {
+
+    const newState = updateState(this.state, entries)
+
+    console.log({ entries, newState, state: this.state });
+    
+    this.state = newState;
+    // entries.forEach((entry) =>
+    //   entry.isIntersecting
+    //     ? entry.target.classList.add("is-view")
+    //     : entry.target.classList.remove("is-view")
+    // );
+  };
+
+  constructor(props: {}) {
+    super(props);
+    this.state = { isInView: ascendingArray.map(() => false) };
+    this.observer = getObserver(this.callback);
+  }
+
+  cells = () =>
+    ascendingArray.map((i) => (
+      <Cell
+        isInView={this.state.isInView[i]}
+        observer={this.observer}
+        i={i}
+        key={i}
+      />
+    ));
+
   render() {
-    return (
-      <>
-      {cells}
-      </>
-    );
+    return <>{this.cells()}</>;
   }
 }
